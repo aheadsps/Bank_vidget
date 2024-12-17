7ca9c470023aebd7c7c4c7e7259d964e14c337b9
Максим Бойков <mboykov6@gmail.com>
2024-12-17 23:07:17 +0300

    update masks.py, and add test_masks.py with positive and negative testing 100% PASSED

--- src/masks.py
+++ src/masks.py
@@ -3,13 +3,14 @@ from typing import Union
 # test_number = 7000792289606361
 
 
-def get_mask_card_number(number_card: Union[int]) -> str:
+def get_mask_card_number(number_card: int) -> str:
     """Функиця, которая принимает номер карты и возвращает его маску
     в формате XXXX XX** **** XXXX"""
-    number_card_str = str(number_card)
-    number_card_mask = f'{number_card_str[0:4]} {number_card_str[4:6]}** **** {number_card_str[-4:]}'
-
-    return number_card_mask
+    if len(str(number_card)) == 16 and number_card > 0 and isinstance(number_card, int):
+        number_card_str = str(number_card)
+        return f"{number_card_str[:4]} {number_card_str[4:6]}** **** {number_card_str[-4:]}"
+    else:
+        raise Exception("Номер карты не верен")
 
 
 # print(get_mask_card_number(test_number))
@@ -17,13 +18,23 @@ def get_mask_card_number(number_card: Union[int]) -> str:
 # test_account = 73654108430135874305
 
 
-def get_mask_account(account_number: Union[int]) -> str:
+def get_mask_account(account_number: int) -> str:
     """Функция, которая прнимает номер счёта и возвращает маску с последними 4 цифрами
     и звездочками перед ними"""
-    account_number_str = str(account_number)
-    account_number_masks = "**" + account_number_str[-4:]
-
-    return account_number_masks
+    if len(str(account_number)) == 20 and account_number > 0 and isinstance(account_number, int):
+        account_number_str = str(account_number)
+        return f"**{account_number_str[-4:]}"
+    else:
+        raise Exception("Номер счета не верен")
 
 
 # print(get_mask_account(test_account))
+
+# def get_mask_account(account_number: int) -> str:
+#     """Принимает на вход номер счета (20цифр)
+#     и возвращает его маску в формате **XXXX"""
+#
+#     if len(str(account_number)) == 20 and account_number > 0 and isinstance(account_number, int):
+#         return f"**{str(account_number)[-4:]}"
+#     else:
+#         raise Exception("Номер счета должен состоять из 20 цифр")
