228d6c0ea840392098d6722957d166117790087c
Maksim Boykov <mboykov6@gmail.com>
2025-02-28 23:36:47 +0300

    function convert_currency_to_rub add to external api

---
+++ src/external_api.py
@@ -0,0 +1,75 @@
+import os
+from typing import Any, Dict, Optional
+
+import requests
+from dotenv import load_dotenv
+
+from utils import read_json
+
+load_dotenv()
+
+
+def convert_currency_to_rub(transaction: Dict[str, Any]) -> Optional[float]:
+    """Функция конвертирует сумму транзакции из USD или EUR в рубли (RUB)"""
+
+    # Проверяем, что транзакция содержит необходимые ключи
+    if "amount" not in transaction["operationAmount"] or "currency" not in transaction["operationAmount"]:
+        print("Транзакция должна содержать ключи 'amount' и 'currency'")
+        return None
+
+    amount = float(transaction["operationAmount"]["amount"])
+    currency = transaction["operationAmount"]["currency"]["code"].upper()  # Приводим валюту к верхнему регистру
+
+    # Если валюта уже в рублях, возвращаем сумму без изменений
+    if currency == "RUB":
+        return float(amount)
+
+    # Поддерживаемые валюты для конвертации
+    if currency not in ["USD", "EUR"]:
+        print(f"Валюта {currency} не поддерживается для конвертации")
+        return None
+
+    # Получаем API-ключ из .env
+    api_key = os.getenv("EXCHANGE_RATE_API_KEY")
+    if not api_key:
+        print("API-ключ не найден в файле .env")
+        return None
+
+    # ссылка для получения курсов валют (USD и EUR относительно RUB)
+    url = "https://api.apilayer.com/exchangerates_data/latest?symbols=USD,EUR&base=RUB"
+
+    try:
+        # Отправляем GET-запрос с API-ключом в заголовке
+        headers = {"apikey": api_key}
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()  # Проверяем, что запрос успешен
+
+        # Извлекаем курсы валют из ответа
+        data = response.json()
+        # print(data)
+        if "rates" not in data:
+            print("Курсы валют не найдены в ответе API.")
+            return None
+
+        exchange_rates = data["rates"]
+
+        # Конвертируем сумму в рубли
+        if currency in exchange_rates:
+            rate = exchange_rates[currency]
+            converted_amount = amount / rate  # Конвертируем сумму в рубли
+            return float(converted_amount)
+        else:
+            print(f"Курс для валюты {currency} не найден.")
+            return None
+
+    except requests.exceptions.RequestException as e:
+        print(f"Ошибка при запросе к API: {e}")
+        return None
+
+
+if __name__ == "__main__":
+    file_path_total = os.path.join("data", "operations.json")
+    transactions = read_json(file_path_total)
+    for transact in transactions[1:2]:
+        result = convert_currency_to_rub(transact)
+        print(f"Сумма в рублях: {result}")
